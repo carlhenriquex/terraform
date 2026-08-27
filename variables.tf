d3d3baf54c2dd7cd5188b77d9ba428ae6f51aa97
Carlos Henrique <carlhenriquedev@gmail.com>
2026-08-27 11:06:51 -0300

    feat: variavel com exemplo de validation

--- variables.tf
+++ variables.tf
@@ -8,3 +8,19 @@ variable "bucket_name" {
     type = string
     description = "Nome do meu bucket"
 }
+
+variable "ambiente" {
+  type = string
+  description = "O ambiente de implantação do projeto."
+  default = "dev"
+  
+  sensitive = false
+  nullable = false
+  ephemeral = false
+
+  # Garante que o usuário só digite uma das três opções válidas
+  validation {
+    condition = contains(["dev", "staging", "prod"], var.ambiente)
+    error_message = "O ambiente deve ser obrigatoriamente 'dev', 'staging' ou 'prod'."
+  }
+}
